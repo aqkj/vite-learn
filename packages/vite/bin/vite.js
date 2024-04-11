66089bf3b06d03fb34cc4f6fefd2d538298a2fea
zouzhiqiang <465633678@qq.com>
2024-04-11 20:19:44 +0800

    upd: vite命令执行文件

--- packages/vite/bin/vite.js
+++ packages/vite/bin/vite.js
@@ -7,34 +7,44 @@ if (!import.meta.url.includes('node_modules')) {
     await import('source-map-support').then((r) => r.default.install())
   } catch (e) {}
 }
-
+// 获取启动时间
 global.__vite_start_time = performance.now()
-
+// 检查是否是存在debug参数
 // check debug mode first before requiring the CLI.
 const debugIndex = process.argv.findIndex((arg) => /^(?:-d|--debug)$/.test(arg))
+// 检查是否存在filter参数
 const filterIndex = process.argv.findIndex((arg) =>
   /^(?:-f|--filter)$/.test(arg),
 )
+// 检查是否存在profile参数
 const profileIndex = process.argv.indexOf('--profile')
-
+// 如果存在debug参数
 if (debugIndex > 0) {
+  // 获取debug参数值
   let value = process.argv[debugIndex + 1]
+  // 如果值不存在或者以-开头,以-开头说明是个参数不是一个value
   if (!value || value.startsWith('-')) {
+    // 默认值
     value = 'vite:*'
   } else {
+    // 使用逗号分隔来支持多个标志支持调试
     // support debugging multiple flags with comma-separated list
     value = value
       .split(',')
       .map((v) => `vite:${v}`)
       .join(',')
   }
+  // 如果环境变量中存在debug,则将参数值value追加进去
   process.env.DEBUG = `${
     process.env.DEBUG ? process.env.DEBUG + ',' : ''
   }${value}`
-
+  // 判断是否存在filter参数
   if (filterIndex > 0) {
+    // 获取filter参数值
     const filter = process.argv[filterIndex + 1]
+    // 如果filter存在并且不以-开头,以-开头说明是个参数不是一个value
     if (filter && !filter.startsWith('-')) {
+      // 将filter参数值赋值给环境变量VITE_DEBUG_FILTER
       process.env.VITE_DEBUG_FILTER = filter
     }
   }
