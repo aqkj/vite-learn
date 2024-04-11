3b5d810c438bc7611e1af9ae3067eaa3505627b7
zouzhiqiang <465633678@qq.com>
2024-04-11 20:55:38 +0800

    upd: 完善bin注释

--- packages/vite/bin/vite.js
+++ packages/vite/bin/vite.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 import { performance } from 'node:perf_hooks'
-
+// 如果不是node_modules中的包 也就是开发模式下
 if (!import.meta.url.includes('node_modules')) {
   try {
+    // source-map-support 用于将错误堆栈映射到源代码
     // only available as dev dependency
     await import('source-map-support').then((r) => r.default.install())
   } catch (e) {}
@@ -50,22 +51,36 @@ if (debugIndex > 0) {
   }
 }
 
+/**
+ * 运行vite
+ */
 function start() {
+  // 引入包
   return import('../dist/node/cli.js')
 }
-
+// 如果存在profile参数 用于启用调试功能
 if (profileIndex > 0) {
+  // 删除profile参数
   process.argv.splice(profileIndex, 1)
+  // 获取profile参数值
   const next = process.argv[profileIndex]
+  // 如果profile参数值存在并且不以-开头,以-开头说明是个参数不是一个value
   if (next && !next.startsWith('-')) {
+    // 删除profile参数值
     process.argv.splice(profileIndex, 1)
   }
+  // nodejs调试
   const inspector = await import('node:inspector').then((r) => r.default)
+  // 创建一个新的会话
   const session = (global.__vite_profile_session = new inspector.Session())
+  // 连接会话
   session.connect()
+  // 启用Profiler
   session.post('Profiler.enable', () => {
+    // 开始Profiler
     session.post('Profiler.start', start)
   })
 } else {
+  // 直接运行
   start()
 }
